6f6ff78fdbeccfeb26eada50368c250b059d8813
Jim Bachesta <j.bachesta@comcast.net>
2018-04-20 10:25:33 -0700

    Second commit

--- git.init.sh
+++ git.init.sh
@@ -1,6 +1,6 @@
 #echo "# bluegreen" >> README.md
 git init
-git config --global use.email "j.bachesta@comcast.net"
+git config --global user.email "j.bachesta@comcast.net"
 git config --global  user.name "Jim Bachesta"
 git add README.md
 git commit -m "first commit"
