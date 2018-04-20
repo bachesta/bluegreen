518b4fd01bd37fbb60d7e97713cb21d2f2203704
Jim Bachesta <j.bachesta@comcast.net>
2018-04-20 10:23:09 -0700

    This is the first commit for this test 4/20/2018

---
+++ git.init.sh
@@ -0,0 +1,8 @@
+#echo "# bluegreen" >> README.md
+git init
+git config --global use.email "j.bachesta@comcast.net"
+git config --global  user.name "Jim Bachesta"
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/bachesta/bluegreen.git
+git push -u origin master
